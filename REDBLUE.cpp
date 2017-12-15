ad7c010dac42ea8d6eec7795a45f5b660dd6e5b6
Satyajit Das <satyajitd1998@gmail.com>
2017-12-15 23:39:20 +0530

    Update REDBLUE.cpp

--- REDBLUE.cpp
+++ REDBLUE.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 const double inf = 10e9;
-
+ 
 double slope_cal(int x1, int y1, int x2, int y2)
 {
     double temp = (double)(y2-y1)/(double)(x2-x1);
     return temp;
 }
-
+ 
 int main()
 {
     int t=0; cin>>t;
@@ -18,13 +18,13 @@ int main()
         cin>>r>>b;
         int n=r+b;
         int X[n], Y[n];
-
+        
         pair<double, int> slope[n]; // double stores the slope and int indicates the point number.
-
+        
         for(int i=0; i<n; i++)
             cin>>X[i]>>Y[i];
         int rem = min(r,b); // represents the minimum number of points to be deleted.
-
+        
         // for each red point are pivot, find out the slope the lines formed it with all other points
         for(int i=0; i<r; i++)
         {
@@ -43,7 +43,7 @@ int main()
                 {
                     slope[j].first = slope_cal(X[i], Y[i], X[j], Y[j]); // calculating slope
                 }
-                if(X[j]>=X[i]) // checking if the jth point is left of the pivot or not
+                if(X[j]>=X[i]) // checking if the jth point is right of the pivot or not
                 {
                     if(j<r)
                         ++red;
@@ -54,18 +54,18 @@ int main()
             // We have finished storing the slopes of all the lines formed by the pivot red point.
             sort(slope, slope+n);
             // We sort the lines according their slope.
-
+            
             //Now, using the pivot point we rotate required line and so we do change count of
             //left and right point. And check for minimum points to be removed after each rotation.
-
+            
             for(int k=0; k<n; k++)
             {
                 rem = min(rem, r-red + blue);
                 rem = min(rem, b-blue + red-1);
-                //r-red : red points to right
-                //b-blue : blue points to right
-                // red-1 : red points to left except pivot
-
+                //r-red : red points to left
+                //b-blue : blue points to left
+                // red-1 : red points to right except pivot
+                
                 //rotation:
                 int j=slope[k].second;
                 if(i==j)
@@ -92,4 +92,4 @@ int main()
         cout<<rem<<endl;
     }
     return 0;
-}
+}   
